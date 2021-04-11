762852548861ed9652a31b9767583634f6db36bd
yash mantri <mantriyash2025@gmail.com>
2021-04-11 20:54:22 +0530

    Fizzbuzz Library Setup Done

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+const stories = storiesOf("Library", module);
+
+stories.add("FizzBuzz", () => {
+  return <div>FizzBuzz</div>;
+});
